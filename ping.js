abeea3d9f1c6577fcfba0b6527624907230a4419
funkyhippo <funkyhippo@f-ck.me>
2021-12-22 21:16:53 -0800

    Add cache buster.

--- ping.js
+++ ping.js
@@ -3,6 +3,6 @@ const { ping } = require("./src/utils");
 if (process.env.NODE_ENV === "production" && process.env.ACCESS_URL) {
   console.log(`Started with self-ping to ${process.env.ACCESS_URL}`);
   setInterval(() => {
-    ping(process.env.ACCESS_URL);
+    ping(process.env.ACCESS_URL + `?cache_bust=${Math.random()}`);
   }, 1000 * 30);
 }
