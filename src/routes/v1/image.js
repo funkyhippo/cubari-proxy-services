e273df622dc8374a9daf0d0a8da36035987e3e93
funkyhippo <funkyhippo@f-ck.me>
2021-12-22 22:34:16 -0800

    Update sensitivity of header filtering.

--- src/routes/v1/image.js
+++ src/routes/v1/image.js
@@ -19,28 +19,29 @@ async function routes(fastify, options) {
   fastify.get("/:url", (request, reply) => {
     const decodedUrl = normalizeUrl(base64UrlDecode(request.params.url));
     const header = getRefererHeader(request.url, decodedUrl);
+    let failRequest = false;
+
     proxy(request.raw, reply.raw, decodedUrl, {
       rewriteRequestHeaders(req, headers) {
         return { ...headers, referer: header };
       },
       rewriteHeaders(headers) {
         if (!(headers["content-type"] || "").startsWith("image")) {
-          return {};
-        } else {
-          const returnHeaders = {};
-          Object.keys(headers)
-            .filter((header) => header.toLowerCase().startsWith("content"))
-            .forEach((header) => {
-              returnHeaders[header] = headers[header];
-            });
-          return returnHeaders;
+          failRqeuest = true;
         }
+        const returnHeaders = {};
+        Object.keys(headers)
+          .filter((header) => header.toLowerCase().startsWith("content"))
+          .forEach((header) => {
+            returnHeaders[header] = headers[header];
+          });
+        return returnHeaders;
       },
       onResponse(req, res, stream) {
-        if (Object.keys(res.getHeaders()).length) {
-          reply.send(stream);
-        } else {
+        if (failRequest) {
           reply.send({ error: "Requested content was not an image." });
+        } else {
+          reply.send(stream);
         }
       },
     });
