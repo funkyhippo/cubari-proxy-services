b0f1512e902619b63d22f571fe602a3edbd2495b
funkyhippo <funkyhippo@f-ck.me>
2022-05-17 02:08:59 -0400

    Move flaresolverr to another remote.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,44 +4,16 @@ module.exports = {
       name: "proxy-services",
       script: "index.js",
       cwd: "./",
-      instances: 3,
+      instances: 4,
       exec_mode: "cluster",
       env: {
         NODE_ENV: "development",
-        FLARESOLVERR_URL: "http://localhost:9192/v1",
         FLARESOLVERR_SESSION: "sess",
       },
       env_production: {
         NODE_ENV: "production",
-        FLARESOLVERR_URL: "http://localhost:9192/v1",
         FLARESOLVERR_SESSION: "sess",
       },
     },
-    {
-      name: "keep-alive",
-      script: "ping.js",
-      cwd: "./",
-      instances: 1,
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
-    },
-    {
-      name: "FlareSolverr",
-      script: "dist/server.js",
-      cwd: "../flaresolverr",
-      instances: 1,
-      env: {
-        PORT: 9192,
-        NODE_ENV: "development",
-      },
-      env_production: {
-        PORT: 9192,
-        NODE_ENV: "production",
-      },
-    }
   ],
 };
